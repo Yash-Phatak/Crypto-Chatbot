69f44b7d988250dad83e28f35f8eba7bdf102cf4
Yash Phatak <jayphatak21@gmail.com>
2023-08-28 21:41:08 +0530

    H

--- Backend/app.py
+++ Backend/app.py
@@ -73,5 +73,13 @@ def realtime():
     answer = get_realtime(text)
     return jsonify(answer)
 
+@app.after_request
+def after_request(response):
+  response.headers.add('Access-Control-Allow-Origin', 'https://crypton-chatbot.vercel.app')
+  response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
+  response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
+  response.headers.add('Access-Control-Allow-Credentials', 'true')
+  return response
+
 if __name__ == "__main__":
     app.run(debug=True)
